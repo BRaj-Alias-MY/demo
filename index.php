edf8925107fa84326393795bd2f6d15bfcd23e4f
vikram <brajclasses@gmail.com>
2018-08-01 19:00:10 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ echo "<h1>List of User/ AMAZON EC2 Server Hosting with PHP + MYSQL </h1>";
 $con = new mysqli('localhost','root','vikram26.26','mytest');
 $sql = "select * from students";
 $result = $con->query($sql);
-echo "<table><tr><th>User ID</th><th>User Name</th></tr>";
+echo "<table border='1'><tr><th>User ID</th><th>User Name</th></tr>";
 while($row=$result->fetch_object())
 {
 echo "<tr>";
